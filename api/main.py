7bb68a921cfa19d431ef471ce003db282991042c
yuvan <yuvanshankar.rocks@gmail.com>
2026-02-10 00:42:12 -0500

    Allowed more methods in CORS

--- api/main.py
+++ api/main.py
@@ -100,8 +100,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=_cors_origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"],
-    allow_headers=["Content-Type", "Accept"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
